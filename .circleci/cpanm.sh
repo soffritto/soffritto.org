02d79aefb4dc6f687b16ebcf847f80d3c4f26673
Nobuo Danjou <danjou@soffritto.org>
2019-01-17 14:55:21 +0900

    fix typo in cpnam.sh

--- .circleci/cpanm.sh
+++ .circleci/cpanm.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cpanm local Carton Module::Install Module::Install::AuthorTests Module::Install::Repository Module::Install::TestBase
+cpanm Carton Module::Install Module::Install::AuthorTests Module::Install::Repository Module::Install::TestBase
--- cpanfile
+++ cpanfile
@@ -1,6 +1,2 @@
-requires 'Module::Install';
-requires 'Module::Install::AuthorTests';
-requires 'Module::Install::Repository';
-requires 'Module::Install::TestBase';
 requires 'Nim',
     url => 'https://github.com/typester/nim/archive/master.zip';
